ef8205659657be6203b4448a9993136900fcc71b
Sandeep <mouryas1306@gmail.com>
2024-11-30 18:54:07 +0530

    db sync off

--- database/login.js
+++ database/login.js
@@ -372,7 +372,7 @@ const clothingimage = async () => {
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-     await sequelize.sync({ alter: true });
+   //  await sequelize.sync({ alter: true });
     console.log('MySQL connected');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
