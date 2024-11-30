00b3cdf520716d0ee8ad5bdfbe4fc4952add08fe
Sandeep <mouryas1306@gmail.com>
2024-11-30 18:31:20 +0530

    new Database Created

--- database/login.js
+++ database/login.js
@@ -1,22 +1,39 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 // Initialize Sequelize (replace with your database configuration)
+// const sequelize = new Sequelize(
+//   {
+//     username: "mgl",
+//       password: "1FKsOyTn9gZdvh4G",
+//       database: "chatapp",
+//       host: "31.220.96.248",
+//       port: 3306,
+//       dialect: "mysql",
+//       "pool": {
+//        "max": 40,
+//        "min": 0,
+//        "acquire": 60000,
+//        "idle": 10000
+//      }
+//     }
+//   );
+
 const sequelize = new Sequelize(
   {
-    username: "mgl",
-      password: "1FKsOyTn9gZdvh4G",
-      database: "chatapp",
-      host: "31.220.96.248",
-      port: 3306,
-      dialect: "mysql",
-      "pool": {
-       "max": 40,
-       "min": 0,
-       "acquire": 60000,
-       "idle": 10000
-     }
-    }
-  );
+        username: "root",
+          password: "cOzbhuWlGXCcHaiweUhMVyGcinrJSGCZ",
+          database: "railway",
+          host: "junction.proxy.rlwy.net",
+          port: 41889,
+          dialect: "mysql",
+          "pool": {
+           "max": 40,
+           "min": 0,
+           "acquire": 60000,
+           "idle": 10000
+         }
+        }
+)
 
 // Define the ClothingItem model
 const ClothingItem = sequelize.define('ClothingItem', {
@@ -355,7 +372,7 @@ const clothingimage = async () => {
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    // await sequelize.sync({ alter: true });
+     await sequelize.sync({ alter: true });
     console.log('MySQL connected');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
